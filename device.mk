86a7a56c212675483f58235f3f41476f16711d43
guaiyihu <guaiyihu@foxmail.com>
2018-09-14 23:25:57 +0800

    whyred: Update GPS from LA.UM.7.2.r1-04500-sdm660.0
    Change-Id: I631cbe8d58f16ebf90d472aa45bcccd6e21a3ad0

--- device.mk
+++ device.mk
@@ -205,15 +205,17 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti \
-    libcurl \
+#   android.hardware.gnss@1.0-impl-qti \
+#   android.hardware.gnss@1.0-service-qti \
+    android.hardware.gnss@1.1-impl-qti \
+    android.hardware.gnss@1.1-service-qti \
+#     libcurl \
     libgnss \
     libgnsspps \
-    libgps.utils \
-    liblocation_api \
-    libloc_core \
-    libloc_pla \
+#    libgps.utils \
+#     liblocation_api \
+#     libloc_core \
+#     libloc_pla \
     libvehiclenetwork-native
 
 # Input
