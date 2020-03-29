e4a4a8838774eeef137129b82d3d145fa7905ab9
Danct12 <danct12@disroot.org>
2020-03-29 20:23:22 +0430

    ubport: Add more packages

--- device.mk
+++ device.mk
@@ -383,6 +383,47 @@ $(call inherit-product, device/halium/whyred/vndk/vndk.mk)
 PRODUCT_PACKAGES += \
     libbt-vendor
 
+#=========== Ubuntu Touch / Halium ============#
+
+# Misc
+PRODUCT_PACKAGES += \
+    libandroid \
+    libandroid_runtime \
+    libion
+
+# Media
+PRODUCT_PACKAGES += \
+    libmedia_omx \
+    drmserver \
+    mediadrmserver \
+    mediaextractor
+
+# Hybris compat libs
+PRODUCT_PACKAGES += \
+    libmedia_compat_layer \
+    libsf_compat_layer \
+    libui_compat_layer
+
+# Droidmedia
+PRODUCT_PACKAGES += \
+    libdroidmedia \
+    minimediaservice \
+    minisfservice \
+    miniafservice
+
+# Ubuntu
+PRODUCT_PACKAGES += \
+    libbiometry_fp_api \
+    libubuntu_application_api
+
+# Enable dynamic partition size
+PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
+
+PRODUCT_PACKAGES += \
+    sensorservice
+
+#=========== Ubuntu Touch / Halium ============#
+
 $(call inherit-product, device/halium/whyred/treble.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/halium/whyred/whyred-vendor.mk)
