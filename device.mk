7d29203d978272b8eec5cd21a3c9daf940ae892d
dopaemon <polarisdp@gmail.com>
2020-05-25 08:34:31 +0000

    RIL : Add Missing Lib     android.hardware.radio@1.0-impl \     android.hardware.radio@1.0-service \     android.hardware.radio.deprecated@1.0 \     android.hardware.radio.deprecated@1.0-impl \     android.hardware.radio.deprecated@1.0-service \

--- device.mk
+++ device.mk
@@ -188,6 +188,11 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
+    android.hardware.radio@1.0-impl \
+    android.hardware.radio@1.0-service \
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.deprecated@1.0-impl \
+    android.hardware.radio.deprecated@1.0-service \
     librmnetctl \
     libxml2 \
     libprotobuf-cpp-full
