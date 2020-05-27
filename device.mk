erfanoabdi/android_device_halium_ubport

ubport: init: overriding services that causes problems
Signed-off-by: Danct12 <danct12@disroot.org>
Signed-off-by: Dreemurrs Quality Check <quality@dreemurrs.local>

device.mk
@@ -22,6 +22,7 @@ PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
 
 # init scripts
 PRODUCT_PACKAGES += \
+    init.disabled.rc \
     init.halium.rc \
     vndk-detect \
     on-post-data.sh

Linux-On-Sdm6Series/device_xiaomi_whyred

device.mk
@@ -298,7 +298,8 @@ PRODUCT_PACKAGES += \
     init.qti.ims.sh \
     init.qti.qseecomd.sh \
     ueventd.rc \
-    init.halium.rc
+    init.halium.rc \
+    init.disabled.rc
 
 # RCS
 PRODUCT_PACKAGES += \