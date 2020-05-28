37a06988304fce5c2bf1fb1560f6d3cf2e3353ec
dopaemon <polarisdp@gmail.com>
2020-05-28 08:13:03 +0000

    whyred: Update user for ims daemons
    Change user for imsdatadaemon, imsrcsd,
    ims_rtp_daemon
    from AID_SYSTEM to AID_RADIO

--- config.fs
+++ config.fs
@@ -1,4 +1,4 @@
-[AID_VENDOR_QTI_DIAG]
+ [AID_VENDOR_QTI_DIAG]
 value:2901
 
 [vendor/bin/wcnss_filter]
@@ -67,39 +67,39 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/imsdatadaemon]
+[vendor/bin/ imsdatadaemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/imsdatadaemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: WAKE_ALARM
 
 [system/vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: WAKE_ALARM
 
