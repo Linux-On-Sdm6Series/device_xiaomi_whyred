5a7465bb931d815ac51e96e5bc1dce66567e4e4a
dopaemon <polarisdp@gmail.com>
2020-05-31 22:16:26 +0700

    let's build android.hardware.camera.provider@2.4-service along with c…
    …amera2
    
    Signed-off-by: sai4041412 <sai404142@gmail.com>
    
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- device.mk
+++ device.mk
@@ -323,10 +323,16 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    libbson \
+    libgui_vendor \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    libbinder_vendor
+    libbinder_vendor \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 #     Snap
 
 # IPv6
