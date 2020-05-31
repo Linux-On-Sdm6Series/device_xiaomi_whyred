d2092768cdbb3f30910d9c881408ae083870496e
dopaemon <polarisdp@gmail.com>
2020-05-31 21:02:30 +0700

    Remove : Frameworks-res

--- device.mk
+++ device.mk
@@ -19,8 +19,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+# PRODUCT_ENFORCE_RRO_TARGETS := \
+#     framework-res
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
