ac35f9566ca12b62714de39b6540d52dd0d64a14
dopaemon <polarisdp@gmail.com>
2020-05-31 22:37:05 +0700

    Support dynamic CameraProvider HAL
    This prevent the message in `dmesg`:
    
        init: Received control message 'interface_start' for 'android.hardware.camera.provider@2.4::ICameraProvider/legacy/0' from pid: 588 (/system/bin/hwservicemanager)
        init: Could not find service hosting interface android.hardware.camera.provider@2.4::ICameraProvider/legacy/0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -249,3 +249,11 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := bin/on-post-data.sh
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := android.hardware.camera.provider@2.4-service.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := root/android.hardware.camera.provider@2.4-service.rc
+LOCAL_MODULE_PATH  := etc/init
+include $(BUILD_PREBUILT)
